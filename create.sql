de26c408e1d63d03a3d5e1e4255a4a1a2c777a95
Anton Semenkov <k3gbdia1@gmail.com>
2021-11-16 20:53:17 +0300

    feat: create database query

---
+++ create.sql
@@ -0,0 +1,28 @@
+create database [Customer Database]
+create table [Customers]
+(
+	CustomerId int identity(1,1) not null constraint [Customer_Id] primary key clustered
+	, [First Name] nvarchar(50)
+	, [Last Name] nvarchar(50) not null
+	, [Phone Number] nvarchar(15) 
+	, Email nvarchar(40) 
+	, Notes ntext not null
+	, [Total Purchases Amount] money
+	, constraint phone_number_cc check ([Phone Number] like '+[1-9]'+ REPLICATE('[0-9]', 13))
+	, constraint email_cc check (Email like '%[A-Z0-9][@][A-Z0-9]%[.][A-Z0-9]%')
+)
+create table [Adresses]
+(
+	AdressId int identity(1,1) not null constraint [Adress_Id] primary key clustered
+	, CustomerId int not null foreign key references Customers(CustomerId)
+	, [Address Line] nvarchar(100) not null
+	, [Address Line2 ] nvarchar(100) 
+	, [Address Type] nvarchar(8) not null constraint address_type_cc check ([Address Type] in ('Shipping', 'Billing')) 
+	, City nvarchar(100) not null
+	, [Postal Code] nvarchar(6) not null
+	, [State] nvarchar(15) not null
+	, Country nvarchar(14) constraint country_cc check (Country in ('United States', 'Canada'))
+)
+
+drop table [Adresses]
+drop table [Customers]
